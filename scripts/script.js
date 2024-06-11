ea21504b5376ffc24ecd81db8c93a44c5b3a6dce
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-10 22:56:14 -0600

    titulos en js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 // alert("hola mundo.");
 console.log("¡Hola, mundo!");
-console.log(142000000 + 229000000);
 
+// operaciones
+console.log(142000000 + 229000000);
 console.log((5 / 2 - 4) * 4);
 console.log(20 % 3);
+// strings
+console.log("Hola soy un string");
+console.log('Hola soy un "string con comillas" y asi es ');
